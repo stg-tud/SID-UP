dfd63e9dffca551639e4c95ea64b488427637838
MisterD <drechsler@cs.tu-darmstadt.de>
2013-01-23 18:01:04 +0100

    fold test now checks intermediate values

--- src/test/FoldTest.scala
+++ src/test/FoldTest.scala
@@ -4,14 +4,16 @@ import testtools.ReactiveLog
 
 object FoldTest extends App {
   val in = EventSource[Int]
-  val out = in.fold(List.empty[Int]){case (list, value) => value :: list }
+  val out = in.fold(List.empty[Int]) { (list, value) => value :: list }
   val log = new ReactiveLog(out);
-  
+
   in << 1
   in << 5
   in << 10
   in << 10
   val lastEvent = in << 2
+
   val result = out.awaitValue(lastEvent);
-  ReactiveLog.assert(result, List(1, 5, 10, 10, 2).reverse);
+  log.assert(Nil, List(1), List(5, 1), List(10, 5, 1), List(10, 10, 5, 1), List(2, 10, 10, 5, 1))
+  ReactiveLog.assert(result, List(2, 10, 10, 5, 1));
 }
