f2399b29f962a12eaee2a91db10eaa1416ab01e0
MisterD <drechsler@cs.tu-darmstadt.de>
2013-02-07 17:23:19 +0100

    simplified snapshot signal a bit

--- src/reactive/impl/SnapshotSignal.scala
+++ src/reactive/impl/SnapshotSignal.scala
@@ -8,7 +8,9 @@ import reactive.EventStreamDependant
 import reactive.SignalDependant
 
 // TODO should not use signal.now, should implement dependency caching equivalent to FunctionalSignal instead
-class SnapshotSignal[A](signal: Signal[A], events: EventStream[_]) extends StatelessSignal[A]("snapshot(" + signal.name + ")on(" + events.name + ")", signal.now) {
+class SnapshotSignal[A](signal: Signal[A], events: EventStream[_]) extends StatelessSignal[A]("snapshot(" + signal.name + ")on(" + events.name + ")", signal.now) with SignalDependant[A] with EventStreamDependant[Any] {
+  signal.addDependant(this);
+  events.addDependant(this);
 
   override def sourceDependencies = events.sourceDependencies
 
@@ -17,52 +19,46 @@ class SnapshotSignal[A](signal: Signal[A], events: EventStream[_]) extends State
   private val waitingForSignal = mutable.Set[Event]()
   private val ignoreForSignal = mutable.Set[Event]()
 
-  private val signalObserver = new SignalDependant[A] {
-    override def notifyEvent(event: Event, value: A, changed: Boolean) {
-      if (events.isConnectedTo(event)) {
-        val shouldEmit = lock.synchronized {
-          if (ignoreForSignal.remove(event)) {
-            false
-          } else {
-            val shouldEmit = waitingForSignal.remove(event);
-            if (!shouldEmit) {
-              waitingForEventStream += (event -> value);
-            }
-            shouldEmit
+  override def notifyEvent(event: Event, value: A, changed: Boolean) {
+    if (events.isConnectedTo(event)) {
+      val shouldEmit = lock.synchronized {
+        if (ignoreForSignal.remove(event)) {
+          false
+        } else {
+          val shouldEmit = waitingForSignal.remove(event);
+          if (!shouldEmit) {
+            waitingForEventStream += (event -> value);
           }
+          shouldEmit
         }
-        if (shouldEmit) {
-          propagate(event, Some(value));
-        }
+      }
+      if (shouldEmit) {
+        propagate(event, Some(value));
       }
     }
   }
-  signal.addDependant(signalObserver);
-  private val eventsObserver = new EventStreamDependant[Any] {
-    def notifyEvent(event: Event, maybeValue: Option[Any]) {
-      maybeValue match {
-        case None =>
-          propagate(event, None);
-          if (signal.isConnectedTo(event)) {
-            lock.synchronized {
-              if (waitingForEventStream.remove(event).isEmpty) {
-                ignoreForSignal += event;
-              }
+  def notifyEvent(event: Event, maybeValue: Option[Any]) {
+    maybeValue match {
+      case None =>
+        propagate(event, None);
+        if (signal.isConnectedTo(event)) {
+          lock.synchronized {
+            if (waitingForEventStream.remove(event).isEmpty) {
+              ignoreForSignal += event;
             }
           }
-        case Some(_) =>
-          if (signal.isConnectedTo(event)) {
-            lock.synchronized {
-              waitingForEventStream.remove(event) match {
-                case Some(value) => propagate(event, Some(value));
-                case None => waitingForSignal += event
-              }
+        }
+      case Some(_) =>
+        if (signal.isConnectedTo(event)) {
+          lock.synchronized {
+            waitingForEventStream.remove(event) match {
+              case Some(value) => propagate(event, Some(value));
+              case None => waitingForSignal += event
             }
-          } else {
-            propagate(event, Some(signal.now));
           }
-      }
+        } else {
+          propagate(event, Some(signal.now));
+        }
     }
   }
-  events.addDependant(eventsObserver);
 }
