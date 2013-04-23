e3d6e4bb312bc8ac78e01fd8bd8ca4f4e2784fbe
MisterD <drechsler@cs.tu-darmstadt.de>
2013-04-23 16:06:39 +0200

    diffusing implementation WIP

--- src/reactive/signals/impl/FlattenSignal.scala
+++ src/reactive/signals/impl/FlattenSignal.scala
@@ -5,7 +5,6 @@ package impl
 import util.MutableValue
 import util.TransactionalTransientVariable
 
-// TODO
 class FlattenSignal[A](val outer: Signal[Signal[A]]) extends {
   var inner = outer.now
 } with SignalImpl[A](outer.sourceDependencies ++ inner.sourceDependencies, inner.now) {
@@ -13,22 +12,35 @@ class FlattenSignal[A](val outer: Signal[Signal[A]]) extends {
 
   outer.addDependant(None, new Signal.Dependant[Signal[A]] {
     override def notify(notification: SignalNotification[Signal[A]]) {
-      if(notification.valueUpdate.changed) {
-        inner.removeDependant(innerDependant)
-      }
       outerNotification.set(notification.transaction, notification);
+      if (notification.valueUpdate.changed) {
+        inner.removeDependant(innerDependant)
+        inner = notification.valueUpdate.newValue
+        inner.addDependant(Some(notification.transaction), innerDependant)
+        if (!inner.isConnectedTo(notification.transaction)) {
+          publish(new SignalNotification(notification.transaction,
+            _sourceDependencies.update(inner.sourceDependencies ++ notification.sourceDependenciesUpdate.newValue),
+            value.update(inner()(notification.transaction))))
+        }
+      }
     }
   })
   val innerDependant = new Signal.Dependant[A] {
     override def notify(notification: SignalNotification[A]) {
-      if(outer.isConnectedTo(notification.transaction)) {
-        
+      if (outer.isConnectedTo(notification.transaction)) {
+        outerNotification.getIfSet(notification.transaction).foreach { outerNotification =>
+          publish(new SignalNotification(
+            notification.transaction,
+            _sourceDependencies.update(outerNotification.sourceDependenciesUpdate.newValue ++ notification.sourceDependenciesUpdate.newValue),
+            value.update(notification.valueUpdate.newValue)))
+        }
+      } else {
+        publish(new SignalNotification(
+          notification.transaction,
+          notification.sourceDependenciesUpdate.applyToMapped(_sourceDependencies, { _ ++ outer.sourceDependencies }),
+          notification.valueUpdate.applyTo(value)))
       }
     }
   }
   inner.addDependant(None, innerDependant)
-
-  def publish() {
-
-  }
 }
