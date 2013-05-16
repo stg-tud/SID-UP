fc09d053d38452d8c8f2f6d5b9125c08f560bf28
MisterD <drechsler@cs.tu-darmstadt.de>
2013-05-16 14:24:40 +0200

    moved tests and ui to separate repositories

--- src/remote/EstablishSignalConnectionData.scala
+++
@@ -1,7 +0,0 @@
-package remote
-
-import java.util.UUID
-import util.SerializationSafe
-import reactive.Signal
-
-case class EstablishSignalConnectionData[A : SerializationSafe](remote : Signal[A], name : String, value : A, sourceDependencies : Map[UUID, UUID])
--- src/ui/ReactiveInput.scala
+++
@@ -1,9 +0,0 @@
-package ui
-import javax.swing.JComponent
-import reactive.Reactive
-import reactive.Signal
-
-trait ReactiveInput[A] extends ReactiveComponent {
-	val value : Signal[A]
-	def setValue(value : A)
-}
