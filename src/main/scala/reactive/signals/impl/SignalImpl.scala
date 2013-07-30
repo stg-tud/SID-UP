5fbb142b4c7b193776be264ddff7caf9a2da5c4e
ragnar <ragnar@rmgk.de>
2013-07-30 20:53:13 +0200

    note pulse name reuse of SignalImpl and Reactive

--- src/main/scala/reactive/signals/impl/SignalImpl.scala
+++ src/main/scala/reactive/signals/impl/SignalImpl.scala
@@ -17,6 +17,7 @@ trait SignalImpl[A] extends ReactiveImpl[A, A, A] with Signal[A] {
   override def flatten[B](implicit evidence: A <:< Signal[B]): Signal[B] = new FlattenSignal(this.asInstanceOf[Signal[Signal[B]]]);
   override def log = new FoldSignal(List(now), changes, ((list: List[A], elem: A) => list :+ elem));
   override def snapshot(when: EventStream[_]): Signal[A] = pulse(when).hold(now);
+  //TODO: has same name as  `Reactive.pulse(transaction: Transaction): Option[P]` but totally different semantics
   override def pulse(when: EventStream[_]): EventStream[A] = new PulseEventStream(this, when);
 
   protected override def getObserverValue(transaction: Transaction, pulseValue: A) = pulseValue
