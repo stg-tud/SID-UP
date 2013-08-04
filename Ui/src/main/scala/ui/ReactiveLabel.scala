2bda5fd5c75d4f015b770237c8023c420b6c6a9a
ragnar <ragnar@rmgk.de>
2013-08-04 12:57:53 +0200

    fix ReactiveLabel not using inital text causes window layout errors

--- Ui/src/main/scala/ui/ReactiveLabel.scala
+++ Ui/src/main/scala/ui/ReactiveLabel.scala
@@ -5,7 +5,7 @@ import reactive.signals.Signal
 import reactive.signals.RoutableVar
 import reactive.Lift.valueToSignal
 
-class ReactiveLabel(initialText: Signal[String]) extends ReactiveComponent(new JLabel()) {
+class ReactiveLabel(initialText: Signal[String]) extends ReactiveComponent(new JLabel(initialText.now)) {
   lazy val text = RoutableVar(initialText);
   observeInEDT(text) { asComponent.setText(_) }
 }
