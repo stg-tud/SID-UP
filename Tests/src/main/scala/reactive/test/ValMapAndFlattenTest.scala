57118111a1328d9da44b8f9cf5628bb9a50d9de6
MisterD <drechsler@cs.tu-darmstadt.de>
2013-09-11 15:33:16 +0200

    not sure if that's more pretty, but it's less to type

--- Tests/src/main/scala/reactive/test/ValMapAndFlattenTest.scala
+++ Tests/src/main/scala/reactive/test/ValMapAndFlattenTest.scala
@@ -8,23 +8,28 @@ import reactive.events.EventStream
 import reactive.signals.Val
 
 class ValMapAndFlattenTest extends FunSuite {
+  // type interference sadly needs a little help here
+  def expect[T](t: T)(tt: => T) {
+    expectResult(t)(tt)
+  }
+  
   test("signal") {
     val signal: Signal[Int] = Var(123)
     val higherSignal: Signal[Signal[Int]] = new Val(signal)
     val evenHigherSignal: Signal[Signal[Signal[Int]]] = new Val(higherSignal)
 
-    expectResult(signal) { higherSignal.flatten[Signal[Int]] }
-    expectResult(higherSignal) { evenHigherSignal.flatten[Signal[Signal[Int]]] }
-    expectResult(signal) { evenHigherSignal.map(_.flatten[Signal[Int]]).flatten[Signal[Int]] }
+    expect(signal) { higherSignal.flatten }
+    expect(higherSignal) { evenHigherSignal.flatten }
+    expect(signal) { evenHigherSignal.map(_.flatten).flatten }
   }
   test("events") {
     val stream: EventStream[Int] = EventSource()
     val higherSignal: Signal[EventStream[Int]] = new Val(stream)
     val evenHigherSignal: Signal[Signal[EventStream[Int]]] = new Val(higherSignal)
 
-    expectResult(stream) { higherSignal.flatten[EventStream[Int]] }
-    expectResult(higherSignal) { evenHigherSignal.flatten[Signal[EventStream[Int]]] }
-    expectResult(stream) { evenHigherSignal.map(_.flatten[EventStream[Int]]).flatten[EventStream[Int]] }
+    expect(stream) { higherSignal.flatten }
+    expect(higherSignal) { evenHigherSignal.flatten }
+    expect(stream) { evenHigherSignal.map(_.flatten).flatten }
   }
 
 }
