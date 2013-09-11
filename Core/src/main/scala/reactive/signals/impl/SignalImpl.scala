0d1adf7413032a018509d4882025bcb582b8a100
MisterD <drechsler@cs.tu-darmstadt.de>
2013-09-11 15:43:58 +0200

    learning scala the hard way

--- Core/src/main/scala/reactive/signals/impl/SignalImpl.scala
+++ Core/src/main/scala/reactive/signals/impl/SignalImpl.scala
@@ -15,7 +15,12 @@ trait SignalImpl[A] extends ReactiveImpl[A, A, A, Signal[A]] with Signal[A] {
   override lazy val changes: EventStream[A] = new ChangesEventStream(this)
   override def map[B](op: A => B): Signal[B] = new MapSignal(this, op)
   override def flatMap[B](op: A => Signal[B]): Signal[B] = map(op).flatten
-  def flatten[R <: Reactive[_, _, _, R]](implicit evidence: A <:< R): R = FlattenSignal(this.asInstanceOf[Signal[R]]);
+  def flatten[R <: Reactive[_, _, _, R]](implicit evidence: A <:< R): R = {
+    // this would be the way to do it without a type cast, but that only costs
+    // pointless performance since <:< is just the identitiy function...
+    // FlattenSignal(this.map(evidence));
+    FlattenSignal(this.asInstanceOf[Signal[R]]);
+  }
   override def log = new FoldSignal(List(now), changes, ((list: List[A], elem: A) => list :+ elem));
   override def snapshot(when: EventStream[_]): Signal[A] = pulse(when).hold(now);
   //TODO: has same name as  `Reactive.pulse(transaction: Transaction): Option[P]` but totally different semantics
