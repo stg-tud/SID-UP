36003dac46339f410b1c3932273c685eae77e865
ragnar <ragnar@rmgk.de>
2013-10-01 20:40:07 +0200

    case class transactions

--- Core/src/main/scala/reactive/Transaction.scala
+++ Core/src/main/scala/reactive/Transaction.scala
@@ -1,15 +1,4 @@
 package reactive
 import java.util.UUID
 
-class Transaction(val sources: scala.collection.Set[UUID]) {
-  val uuid = UUID.randomUUID();
-  override def equals(obj : Any) = {
-    obj.isInstanceOf[Transaction] && obj.asInstanceOf[Transaction].uuid.equals(uuid)
-  }
-  override def hashCode() = {
-    uuid.hashCode()
-  }
-  override def toString() = {
-    "Transaction(id="+uuid+",sources="+sources+")"
-  }
-}
+case class Transaction(sources: scala.collection.Set[UUID], uuid: UUID = UUID.randomUUID())
