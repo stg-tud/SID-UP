1b939c42fb593fd633d4771b2335abe0c3bb5382
ragnar <ragnar@rmgk.de>
2013-11-25 21:01:38 +0100

    many sources benchmark

---
+++ Benchmark/src/main/scala/benchmark/ManySources.scala
@@ -0,0 +1,43 @@
+package benchmark
+
+import scala.language.higherKinds
+
+class ManySources[GenSig[Int], GenVar[Int] <: GenSig[Int]](size: Int, val wrapper: ReactiveWrapper[GenSig, GenVar]) extends SimpleWaitingTest[GenSig, GenVar] {
+
+  import wrapper._
+
+  val sourceSize = size * 1000
+
+  val sourceList = Range(0, sourceSize).map(_ => makeVar(0))
+
+  val sourceListA = transpose(Range(0, sourceSize).map(_ => makeVar(0)))
+  val sourceListB = transpose(Range(0, sourceSize).map(_ => makeVar(0)))
+  val sourceListC = transpose(Range(0, sourceSize).map(_ => makeVar(0)))
+  
+  val first = sourceList.head
+  val transposed = transpose(sourceList)
+  val secondA = StructureBuilder.makeChain(size, wrapper, {
+    val netSig = map(transposed){v: Seq[Int] =>
+      Simulate.network()
+      Seq(v.sum + 1000)
+    }
+    map(transpose(Seq(netSig,sourceListA))){_.head.head}
+  })
+
+  val secondB = StructureBuilder.makeFan(size, wrapper, {
+    val netSig = map(transposed){v: Seq[Int] =>
+      Simulate.network()
+      Seq(v.sum + 1000)
+    }
+    map(transpose(Seq(netSig,sourceListA))){_.head.head}
+  })
+
+  val secondC = map(transposed){v: Seq[Int] =>
+    Simulate.network()
+    v.sum + 1000
+  }
+
+  val last = map(transpose(Seq(secondA, secondB, secondC)))(vs => {Simulate.network(); vs.sum})
+
+
+}
--- Benchmark/src/main/scala/benchmark/ThreeHosts.scala
+++ Benchmark/src/main/scala/benchmark/ThreeHosts.scala
@@ -2,19 +2,19 @@ package benchmark
 
 import scala.language.higherKinds
 
-class ThreeHosts[GenSig[Int], GenVar[Int] <: GenSig[Int]](length: Int, val wrapper: ReactiveWrapper[GenSig, GenVar]) extends SimpleWaitingTest[GenSig, GenVar] {
+class ThreeHosts[GenSig[Int], GenVar[Int] <: GenSig[Int]](size: Int, val wrapper: ReactiveWrapper[GenSig, GenVar]) extends SimpleWaitingTest[GenSig, GenVar] {
 
   import wrapper._
 
   val first = makeVar(-10)
-  val secondA = StructureBuilder.makeChain(length, wrapper, {
+  val secondA = StructureBuilder.makeChain(size, wrapper, {
     map(first){v: Int =>
       Simulate.network()
       v + 1000
     }
   })
 
-  val secondB = StructureBuilder.makeFan(length, wrapper, {
+  val secondB = StructureBuilder.makeFan(size, wrapper, {
     map(first){v: Int =>
       Simulate.network()
       v + 1000
