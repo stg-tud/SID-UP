bccc957b2ac0b49d6cd46fcc9cc570e9730ae3b8
MisterD <drechsler@cs.tu-darmstadt.de>
2013-12-13 10:49:34 +0100

    fixed compile error

--- Core/src/main/scala/reactive/signals/TransposeSignal.scala
+++ Core/src/main/scala/reactive/signals/TransposeSignal.scala
@@ -8,7 +8,7 @@ import reactive.signals.impl.DependentSignalImpl
  * Takes a sequence of signals and turns it into a signal containing a sequence of the original values
  */
 class TransposeSignal[A](signals: Seq[Signal[A]]) extends {
-  override val dependencies = signals.toSet[Reactive[_, _, _]]
+  override val dependencies = signals.toSet[Reactive.Dependency]
 } with DependentSignalImpl[Seq[A]] with MultiDependentReactive {
   override def reevaluateValue(transaction: Transaction) = signals.map(_.value(transaction))
 }
