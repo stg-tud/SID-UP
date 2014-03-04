8ce800017869b2c381d661e0855f65c31dc8b116
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2014-03-04 22:35:17 +0100

    WhiteboardServer can now handle multiple clients

--- CaseStudies/SharedReactiveWhiteboard/src/main/scala/whiteboard/WhiteboardServer.scala
+++ CaseStudies/SharedReactiveWhiteboard/src/main/scala/whiteboard/WhiteboardServer.scala
@@ -4,6 +4,7 @@ import java.rmi.server.UnicastRemoteObject
 import reactive.remote.RemoteReactives
 import whiteboard.figures.Shape
 import java.rmi.Naming
+import reactive.events.impl.DynamicMergeStream
 
 object WhiteboardServer extends App {
   @remote trait RemoteWhiteboard {
@@ -11,17 +12,20 @@ object WhiteboardServer extends App {
   }
   class RemoteWhiteboardImpl extends UnicastRemoteObject with RemoteWhiteboard {
     override def connectShapes(shapeStreamIdentifier: String): String = {
-      val shapeStream = RemoteReactives.lookupEvent[Shape](shapeStreamIdentifier)
-      val allShapes = shapeStream.log
+      val newClientShapeStream = RemoteReactives.lookupEvent[Shape](shapeStreamIdentifier)
+      allClientsShapeStream.addEvents(newClientShapeStream)
 
-      RemoteReactives.rebind("shapeList", allShapes)
-      "shapeList"
+      "shapes"
     }
   }
 
   try { java.rmi.registry.LocateRegistry.createRegistry(1099) }
   catch { case _: Exception => println("registry already initialised") }
   Naming.rebind("remoteWhiteboard", new RemoteWhiteboardImpl)
+
+  val allClientsShapeStream = new DynamicMergeStream[Shape]()
+  val shapes = allClientsShapeStream.log
+  RemoteReactives.rebind("shapes", shapes)
 
   while (true) {
     Thread.sleep(1000)
