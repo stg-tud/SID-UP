346bbc463019a4ea6043204575087dd470e3e288
ragnar <ragnar@rmgk.de>
2014-03-13 21:15:19 +0100

    TransposeEventStream now accepts Iterable instead of Seq

--- Core/src/main/scala/reactive/events/TransposeEventStream.scala
+++ Core/src/main/scala/reactive/events/TransposeEventStream.scala
@@ -8,13 +8,12 @@ import reactive.events.impl.DependentEventStreamImpl
 /**
  * Takes a sequence of events and turns it into an event firing every time one of the original events changes
  */
-class TransposeEventStream[A](events: Signal[Seq[EventStream[A]]]) extends DependentEventStreamImpl[Seq[A]] with DynamicDependentReactive {
-  override protected def reevaluate(transaction: Transaction): Option[Seq[A]] =
+class TransposeEventStream[A](events: Signal[Iterable[EventStream[A]]]) extends DependentEventStreamImpl[Iterable[A]] with DynamicDependentReactive {
+  override protected def reevaluate(transaction: Transaction): Option[Iterable[A]] = {
     // get the flat list of pulses, if none changed, we also propagate no change
-    events.value(transaction).map(_.pulse(transaction)).flatten match {
-      case Seq()    => None
-      case notEmpty => Some(notEmpty)
-    }
+    val pulses = events.value(transaction).map(_.pulse(transaction)).flatten
+    if (pulses.isEmpty) None else Some(pulses)
+  }
 
   override protected def dependencies(transaction: Transaction): Set[Reactive[_, _]] = events.value(transaction).toSet + events
 }
