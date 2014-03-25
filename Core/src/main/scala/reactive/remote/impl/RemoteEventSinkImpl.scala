1a699a376b4665732d374374c0ba914385c209cc
ragnar <ragnar@rmgk.de>
2014-03-25 12:53:28 +0100

    temporarily remove remote reactives

--- Core/src/main/scala/reactive/remote/impl/RemoteEventSinkImpl.scala
+++
@@ -1,6 +0,0 @@
-package reactive.remote.impl
-
-import reactive.remote.RemoteDependency
-import reactive.events.impl.EventStreamImpl
-
-class RemoteEventSinkImpl[A](dependency: RemoteDependency[A]) extends RemoteSinkImpl[A](dependency) with EventStreamImpl[A] {}
--- Core/src/main/scala/reactive/remote/impl/RemoteEventSourceImpl.scala
+++
@@ -1,8 +0,0 @@
-package reactive.remote.impl
-
-import reactive.signals.Signal
-import reactive.remote.RemoteSignalDependency
-import reactive.Transaction
-import reactive.events.{EventStream, EventSource}
-
-class RemoteEventSourceImpl[P](val dependency: EventStream[P]) extends RemoteSourceImpl[P] {}
