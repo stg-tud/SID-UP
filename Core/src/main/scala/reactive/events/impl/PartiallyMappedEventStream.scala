71e462de8a9b60668b9811d431dd49d67555f863
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-04-09 16:51:36 +0200

    added a collect function to event streams complementing map for partial functions in the same way it is done for collections

---
+++ Core/src/main/scala/reactive/events/impl/PartiallyMappedEventStream.scala
@@ -0,0 +1,9 @@
+package reactive
+package events
+package impl
+
+import reactive.impl.SingleDependentReactive
+
+class PartiallyMappedEventStream[A, B](val dependency: EventStream[B], private val op: PartialFunction[B, A]) extends DependentEventStreamImpl[A] with SingleDependentReactive {
+  protected def reevaluate(transaction: Transaction): Option[A] = dependency.pulse(transaction).filter(op.isDefinedAt(_)).map(op)
+}
