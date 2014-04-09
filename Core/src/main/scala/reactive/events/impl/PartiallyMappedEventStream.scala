17d8712f69f6fce402ffd127fd47428e3165f1dc
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-04-09 17:16:32 +0200

    actually can use collect directly here in that case >.<

--- Core/src/main/scala/reactive/events/impl/PartiallyMappedEventStream.scala
+++ Core/src/main/scala/reactive/events/impl/PartiallyMappedEventStream.scala
@@ -5,5 +5,5 @@ package impl
 import reactive.impl.SingleDependentReactive
 
 class PartiallyMappedEventStream[A, B](val dependency: EventStream[B], private val op: PartialFunction[B, A]) extends DependentEventStreamImpl[A] with SingleDependentReactive {
-  protected def reevaluate(transaction: Transaction): Option[A] = dependency.pulse(transaction).filter(op.isDefinedAt(_)).map(op)
+  protected def reevaluate(transaction: Transaction): Option[A] = dependency.pulse(transaction).collect(op)
 }
