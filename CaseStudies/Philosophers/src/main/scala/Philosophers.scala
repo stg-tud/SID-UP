ff9ed0e593e041a6a712f44e28cd9a9f54537172
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-05-05 15:06:58 +0200

    prettied up a bit

--- CaseStudies/Philosophers/src/main/scala/Philosophers.scala
+++ CaseStudies/Philosophers/src/main/scala/Philosophers.scala
@@ -5,122 +5,154 @@ import reactive.TransactionBuilder
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits._
 import reactive.signals.TransposeSignal
-import scala.concurrent.stm.atomic
-import scala.concurrent.stm.Txn
+import scala.concurrent.stm._
+import scala.concurrent.duration._
 import reactive.Reactive
 import reactive.events.EventStream
 import java.io.BufferedInputStream
+import scala.concurrent.Await
+import java.util.concurrent.TimeoutException
 
-object Philosophers extends App {
-  def log(msg: String) = {
-    println("[" + Thread.currentThread().getName() + " @ " + System.currentTimeMillis() + "] " + msg)
-  }
+// ======= FORK IMPLEMENTATION =======
 
-  class Fork(val id: Int) {
-    val in = Var[Set[Signal[Option[Philosopher]]]](Set())
-    private val requestStates = new TransposeSignal(in)
-    private val requests = requestStates.map(_.flatten)
-    requests.changes.filter(_.size > 1).observe(reqs => log("Multiple owners requested for " + this + ": " + reqs))
-    val owner = requests.map(_.headOption)
-    //    owner.observe { owner =>
-    //      log(this + " now " + (owner match {
-    //        case Some(x) => "owned by " + x
-    //        case None => "free"
-    //      }))
-    //    }
-    override def toString(): String = "Fork " + id
-    def status = this + " requested by " + requests.now + " owned by " + owner.now
-  }
+class Fork(val id: Int) {
+  // input
+  val in = Var[Set[Signal[Option[Philosopher]]]](Set())
 
-  // a philosopher is eating whenever she owns both forks
+  // intermediate
+  val requestStates = new TransposeSignal(in)
+  val requests = requestStates.map(_.flatten)
+
+  // output
+  val owner = requests.map(_.headOption)
+
+  // infrastructure
+  override def toString(): String = "Fork " + id
+}
+
+// ======= PHILOSOPHER IMPLEMENTATION =======
+
+object Philosopher {
+  // function: a philosopher is eating whenever she owns both forks
   val calculateEating: (Philosopher, Option[Philosopher], Option[Philosopher]) => Boolean = (id, left, right) => (left, right) match {
     case (Some(`id`), Some(`id`)) => true
     case _ => false
   }
+}
 
-  class Philosopher(val id: Int, val leftFork: Fork, val rightFork: Fork) {
-    val tryingToEat = Var(false)
-    val request = tryingToEat.map(_ match {
-      case false => None
-      case true => Some(this)
-    })
+class Philosopher(val id: Int, val leftFork: Fork, val rightFork: Fork, val eatingDuration: Int) {
+  // input
+  val tryingToEat = Var(false)
 
-    leftFork.in << leftFork.in.now + request
-    rightFork.in << rightFork.in.now + request
+  // intermediate
+  val request = tryingToEat.map(_ match {
+    case false => None
+    case true => Some(this)
+  })
 
-    // connect this philosopher's eating state with her forks
-    val isEating = signal3(calculateEating)(this, leftFork.owner, rightFork.owner)
-    // print a notification whenever this philosopher starts or stops eating
-    //    isEating.observe { value =>
-    //      log(this + " is " + (if (value) "now" else "no longer") + " eating");
-    //    }
+  // connect input to forks
+  leftFork.in << leftFork.in.now + request
+  rightFork.in << rightFork.in.now + request
 
-    // kill-switch
-    private var killed = false
-    def kill() = killed = true
-    // acting thread: The philosopher repeatedly tries to acquire both forks until she is killed
-    var working = true
-    Future {
-      println(this + ": Thread id " + Thread.currentThread().getName())
-      while (!killed) {
-        if (atomic { tx =>
-          if (leftFork.owner.now.isEmpty) {
-            if (rightFork.owner.now.isEmpty) {
-              tryingToEat << true
-              true
-            } else {
-              false
-            }
-          } else {
-            false
-          }
-        }) {
+  // connect output from forks
+  val isEating = signal3(Philosopher.calculateEating)(this, leftFork.owner, rightFork.owner)
 
-          Thread.sleep(100)
+  // infrastructure
+  override def toString(): String = "Philosopher " + id
 
-          // release forks
-          tryingToEat << false
-        }
+  // behavior
+  def eatOnce() = {
+    atomic { tx =>
+      // await free forks
+      if (leftFork.owner.now.isDefined || rightFork.owner.now.isDefined) {
+        retry(tx)
       }
-      working = false
+      // try to take forks
+      tryingToEat << true
     }
 
-    override def toString(): String = "Philosopher " + id
-    def status = this + " trying to eat: " + tryingToEat.now + ", requesting: " + request.now + ", eating: " + isEating.now
-  }
+    // "eat"
+    Thread.sleep(eatingDuration)
 
-  // size of the table == number of forks == number of philosophers
-  val n = if (args.length < 1) {
-    println("Using default table size 3. Supply an integer as first argument to customize.");
+    // release forks
+    tryingToEat << false
+  }
+}
+
+object Philosophers extends App {
+  // ======= PARAMETER PARSING =======
+  val sizeOfTable = if (args.length < 1) {
+    println("Using default table size 3. Supply an integer number as first program argument to customize.");
     3
   } else {
     Integer.parseInt(args(0))
   }
 
-  // create forks
-  val fork = for (i <- 0 until n) yield new Fork(i)
-  // create and run philosophers
-  val philosopher = for (i <- 0 until n) yield new Philosopher(i, fork(i), fork((i + 1) % 3))
+  val eatingDuration = 100
 
-  def dumpStatus() = {
-    println("Status Forks:")
-    fork.foreach { fork =>
-      println(" " + fork.status)
-    }
-    println("Status Philosphers:")
-    philosopher.foreach { phil =>
-      println(" " + phil.status)
-    }
+  // ======= TABLE SETUP =======
+  println("Setting up table with " + sizeOfTable + " forks and philosophers")
+
+  // create forks
+  val fork = for (i <- 0 until sizeOfTable) yield new Fork(i)
+  // create and run philosophers
+  val philosopher = for (i <- 0 until sizeOfTable) yield new Philosopher(i, fork(i), fork((i + 1) % 3), eatingDuration)
+
+  // ======= OBSERVATION SETUP =======
+  def log(msg: String) = {
+    println("[" + Thread.currentThread().getName() + " @ " + System.currentTimeMillis() + "] " + msg)
   }
 
+  // ---- fork state observation ----
+  //  fork.foreach(_.owner.observe { owner =>
+  //    log(this + " now " + (owner match {
+  //      case Some(x) => "owned by " + x
+  //      case None => "free"
+  //    }))
+  //  })
+
+  // ---- philosopher state observation ----
+  //  philosopher.foreach {
+  //    _.isEating.observe { value =>
+  //        log(this + " is " + (if (value) "now" else "no longer") + " eating");
+  //      }
+  //  }
+
+  // ---- table state observation ----
   val allEating = new TransposeSignal(philosopher.map(p => (p.isEating.map(_ -> p)))).map(_.filter(_._1).map(_._2))
   allEating.changes.filter(!_.isEmpty).observe(eating => log("Now eating: " + eating))
 
-  // kill all philosophers
-  System.in.read()
-  philosopher.foreach(_.kill())
+  // ======= RUN TIME HANDLING =======
+  // start simulation
+  private var killed = false
+  println("Starting simulation. Hit <Enter> to terminate!")
+  val threads = philosopher.map { phil =>
+    phil ->
+      Future {
+        println(phil + ": using " + phil.leftFork + " and " + phil.rightFork + " on thread" + Thread.currentThread().getName())
+        while (!killed) {
+          phil.eatOnce
+        }
+        log(phil + " dies.")
+      }
+  }
 
+  // wait for keyboard input
+  System.in.read()
+
+  // kill all philosophers
   println("Received Termination Signal, Terminating...")
-  Thread.sleep((n + 1) * 1000)
-  println("Philosophers that have not terminated: " + philosopher.filter(_.working))
+  killed = true
+  val maxCompletionTime = System.currentTimeMillis() + sizeOfTable * (eatingDuration + 10)
+
+  // collect forked threads to check termination
+  threads.foreach {
+    case (phil, thread) => try {
+      import scala.language.postfixOps
+      Await.ready(thread, (maxCompletionTime - System.currentTimeMillis()) millis)
+      println(phil + " terminated.")
+    } catch {
+      case te: TimeoutException => println(phil + " failed to terminate!")
+    }
+  }
 }
