f2262291bd4329fd4c96203948005fa3a68fe2a3
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-05-06 10:51:51 +0200

    merged DeltaEventStream to STM branch

--- Core/src/main/scala/reactive/events/impl/DeltaEventStream.scala
+++ Core/src/main/scala/reactive/events/impl/DeltaEventStream.scala
@@ -4,12 +4,14 @@ package impl
 
 import reactive.signals.Signal
 import reactive.impl.SingleDependentReactive
+import scala.concurrent.stm._
 
 class DeltaEventStream[A](val dependency: Signal[A]) extends DependentEventStreamImpl[(A, A)] with SingleDependentReactive {
-  private var lastValue: A = dependency.now
+  private val lastValue = Ref(dependency.now)
   protected def reevaluate(transaction: Transaction): Option[(A, A)] = {
-    val oldValue = lastValue
-    lastValue = dependency.pulse(transaction).get
-    Some(oldValue -> lastValue)
+    atomic { tx =>
+      val newValue = dependency.pulse(transaction).asOption.get
+      Some(lastValue.swap(newValue)(tx) -> newValue)
+    }
   }
 }
