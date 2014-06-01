7370e9609da37e44244b81379c02a79b08ec6f5c
ragnar <ragnar@rmgk.de>
2014-06-01 22:17:09 +0200

    make MessageBuffer implementation readable

--- Core/src/test/scala/reactive/testtools/MessageBuffer.scala
+++ Core/src/test/scala/reactive/testtools/MessageBuffer.scala
@@ -1,56 +1,35 @@
 package reactive.testtools
 
-import reactive.signals.impl.SignalImpl
 import reactive.signals.Signal
-import scala.collection.mutable
 import reactive.Transaction
 import reactive.impl.SingleDependentReactive
 import reactive.signals.impl.DependentSignalImpl
 import scala.concurrent.stm.InTxn
+import java.util.concurrent.Semaphore
+
 
 class MessageBuffer[A](val id: String, override val dependency: Signal[A], tx: InTxn) extends SingleDependentReactive(tx) with DependentSignalImpl[A] {
-  def log(msg: String) { println("[" + id + " @ " + Thread.currentThread().getName() + "] " + msg) }
-  private object parking;
-  private var parkingState = 0
-  log("Awaiting message")
 
-  override def ping(transaction: Transaction, sourceDependenciesChanged: Boolean, pulsed: Boolean) {
-    parking.synchronized {
-      log("Received message")
-      if (parkingState != 0) throw new IllegalStateException
-      parkingState = 1
-      parking.notifyAll
-      while (parkingState == 1) {
-        parking.wait
-      }
-      log("Delivering message")
-    }
+  def log(msg: String) { println(s"[$id @ ${Thread.currentThread().getName()}] $msg") }
+
+  private val delivery = new Semaphore(0)
+  private val deliveryConfirmation = new Semaphore(0)
+
+  override def ping(transaction: Transaction, sourceDependenciesChanged: Boolean, pulsed: Boolean) = {
+    log("Received message")
+    delivery.acquire()
+    log("Delivering message")
     doReevaluation(transaction, sourceDependenciesChanged, pulsed)
-    parking.synchronized {
-      log("Delivered message")
-      while (parkingState != 2) {
-        parking.wait
-      }
-      parkingState = 0
-      parking.notifyAll
-      log("Awaiting message")
-    }
+    log("Delivered message")
+    deliveryConfirmation.release()
   }
 
-  def releaseQueue() {
-    parking.synchronized {
-      log("Send command")
-      while (parkingState != 1) {
-        parking.wait
-      }
-      parkingState = 2
-      parking.notifyAll
-      log("Awaiting delivery confirmation")
-      while (parkingState == 2) {
-        parking.wait
-      }
-      log("Delivery confirmed.")
-    }
+  def releaseQueue() = {
+    log("Release queue")
+    delivery.release()
+    log("Awaiting confirmation")
+    deliveryConfirmation.acquire()
+    log("Delivery confirmed")
   }
 
   override protected def reevaluateValue(tx: InTxn): A = dependency.now(tx)
