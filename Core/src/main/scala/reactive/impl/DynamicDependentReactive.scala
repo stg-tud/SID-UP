cbc171bac967909e908214ac1fb5472a1599b6b0
ragnar <ragnar@rmgk.de>
2014-06-01 22:14:53 +0200

    make dynamic dependent reactive more pretty (maybe)

--- Core/src/main/scala/reactive/impl/DynamicDependentReactive.scala
+++ Core/src/main/scala/reactive/impl/DynamicDependentReactive.scala
@@ -5,14 +5,14 @@ import java.util.UUID
 import com.typesafe.scalalogging.slf4j.Logging
 import scala.concurrent.stm._
 
-abstract class DynamicDependentReactive(tx: InTxn) extends Logging {
-  self: DependentReactive[_] with ReactiveImpl[_, _] =>
+abstract class DynamicDependentReactive(constructionTransaction: InTxn) extends Logging {
+  self: DependentReactive[_] =>
 
   protected def dependencies(tx: InTxn): Set[Reactive[_, _]]
 
   private val lastDependencies = {
-    val depts = dependencies(tx)
-    depts.foreach { _.addDependant(tx, this) }
+    val depts = dependencies(constructionTransaction)
+    depts.foreach { _.addDependant(constructionTransaction, this) }
     Ref(depts)
   }
 
@@ -20,47 +20,65 @@ abstract class DynamicDependentReactive(tx: InTxn) extends Logging {
   private val anyPulse: Ref[Boolean] = Ref(false)
   private val hasPulsedLocal: Ref[Boolean] = Ref(false)
 
-  override def ping(transaction: Transaction, sourceDependenciesChanged: Boolean, pulsed: Boolean): Unit = {
-    val tx = transaction.stmTx
-    val pulseNow = tx.synchronized {
-      if (hasPulsedLocal()(tx)) {
-        logger.trace(s"$name received orphaned notification after having pulsed; ignoring notification")
-        false
-      } else {
-        anyPulse.transform(_ || pulsed)(tx)
-        anyDependenciesChanged.transform(_ || sourceDependenciesChanged)(tx)
+  /**
+   * recalculates the current dependencies,
+   * updates the cached dependencies
+   * and handles registrations and unregistrations of changed dependencies
+   */
+  private def handleDependencyChanges(tx: InTxn) = {
+    val newDependencies = dependencies(tx)
+    val oldDependencies = lastDependencies.swap(newDependencies)(tx)
+    val unsubscribe = oldDependencies.diff(newDependencies)
+    val subscribe = newDependencies.diff(oldDependencies)
+    unsubscribe.foreach { dep =>
+      dep.removeDependant(tx, this)
+    }
+    subscribe.foreach { dep =>
+      dep.addDependant(tx, this)
+    }
+    if (!unsubscribe.isEmpty || !subscribe.isEmpty) {
+      anyDependenciesChanged.set(true)(tx)
+    }
+  }
 
+  /**
+   * @return true iff all dependencies have pulsed and this reactive has not yet pulsed
+   */
+  private def checkIfDependenciesPulsed(transaction: Transaction, sourceDependenciesChanged: Boolean, pulsed: Boolean) = {
+    val tx = transaction.stmTx
+    tx.synchronized {
+      if (hasPulsedLocal()(tx)) {
+        logger.error(s"$this received orphaned notification after having pulsed; ignoring notification")
+        false
+      }
+      else {
+        if (sourceDependenciesChanged) anyDependenciesChanged.set(true)(tx)
         if (pulsed) {
-          val newDependencies = dependencies(tx)
-          val oldDependencies = lastDependencies.swap(newDependencies)(tx)
-          val unsubscribe = oldDependencies.diff(newDependencies)
-          val subscribe = newDependencies.diff(oldDependencies)
-          unsubscribe.foreach { dep =>
-            dep.removeDependant(tx, this)
-          }
-          subscribe.foreach { dep =>
-            dep.addDependant(tx, this)
-          }
-          if (!unsubscribe.isEmpty || !subscribe.isEmpty) {
-            anyDependenciesChanged.set(true)(tx)
-          }
+          anyPulse.set(true)(tx)
+          handleDependencyChanges(tx)
         }
-        
+
         val waitingFor = lastDependencies()(tx).filter(dependency => dependency.isConnectedTo(transaction) && !dependency.hasPulsed(tx))
         if (waitingFor.isEmpty) {
           Txn.beforeCommit(hasPulsedLocal.set(false)(_))(tx)
           hasPulsedLocal.set(true)(tx)
           true
-        } else {
-          logger.trace(s"$name still waits for updates from $waitingFor)")
+        }
+        else {
+          logger.trace(s"$this still waits for updates from $waitingFor)")
           false
         }
       }
     }
+  }
 
+  override def ping(transaction: Transaction, sourceDependenciesChanged: Boolean, pulsed: Boolean): Unit = {
+    val tx = transaction.stmTx
+    val pulseNow = checkIfDependenciesPulsed(transaction, sourceDependenciesChanged, pulsed)
     if (pulseNow) {
-      val dependenciesChangedFlag = tx.synchronized(anyDependenciesChanged.swap(false)(tx))
-      val pulsedFlag = tx.synchronized(anyPulse.swap(false)(tx))
+      val (dependenciesChangedFlag, pulsedFlag) = tx.synchronized {
+        (anyDependenciesChanged.swap(false)(tx), anyPulse.swap(false)(tx))
+      }
       doReevaluation(transaction, dependenciesChangedFlag, pulsedFlag)
     }
   }
