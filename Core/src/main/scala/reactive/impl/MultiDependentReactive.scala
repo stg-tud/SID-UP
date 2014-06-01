586e4c67c187cacff6b4ab38e30ef975ea4af33c
ragnar <ragnar@rmgk.de>
2014-06-01 22:15:24 +0200

    fix multi dependet reactive synchronization

--- Core/src/main/scala/reactive/impl/MultiDependentReactive.scala
+++ Core/src/main/scala/reactive/impl/MultiDependentReactive.scala
@@ -5,41 +5,57 @@ import java.util.UUID
 import com.typesafe.scalalogging.slf4j.Logging
 import scala.concurrent.stm._
 
-abstract class MultiDependentReactive(tx: InTxn) extends Logging {
+abstract class MultiDependentReactive(constructionTransaction: InTxn) extends Logging {
   self: DependentReactive[_] =>
 
   protected val dependencies: Set[Reactive.Dependency]
-  dependencies.foreach { _.addDependant(tx, this) }
+  dependencies.foreach { _.addDependant(constructionTransaction, this) }
 
-  private val pendingNotifications: Ref[Int] = Ref(0)
+  private val pendingNotifications: Ref[Int] = Ref(-1)
   private val anyDependenciesChanged: Ref[Boolean] = Ref(false)
   private val anyPulse: Ref[Boolean] = Ref(false)
 
-  override def ping(transaction: Transaction, sourceDependenciesChanged: Boolean, pulsed: Boolean): Unit = {
-    //    synchronized {
-    val stillPending = tx.synchronized {
-      pendingNotifications.transformAndGet { previous =>
-        if (previous == 0) {
-          dependencies.count(_.isConnectedTo(transaction)) - 1
-        } else {
-          previous - 1
-        }
-      }(transaction.stmTx)
-    }
-    tx.synchronized(anyDependenciesChanged.transform(_ || sourceDependenciesChanged)(transaction.stmTx))
-    tx.synchronized(anyPulse.transform(_ || pulsed)(transaction.stmTx))
+  private def updateAndGetStillPending(transaction: Transaction) = {
+    pendingNotifications.transformAndGet { previous =>
+      if (previous == -1) {
+        Txn.beforeCommit(pendingNotifications.set(-1)(_))(transaction.stmTx)
+        dependencies.count(_.isConnectedTo(transaction)) - 1
+      }
+      else {
+        previous - 1
+      }
+    }(transaction.stmTx)
+  }
 
+  private def checkIfDependenciesPulsed(transaction: Transaction) = {
+    val stillPending = updateAndGetStillPending(transaction)
     if (stillPending == 0) {
-      logger.trace(s"$this received last remaining notification for transaction ${transaction}, starting reevaluation")
-      val dependenciesChangedFlag = tx.synchronized(anyDependenciesChanged.swap(false)(transaction.stmTx))
-      val pulsedFlag = tx.synchronized(anyPulse.swap(false)(transaction.stmTx))
-      doReevaluation(transaction, dependenciesChangedFlag, pulsedFlag)
-    } else if (stillPending < 0) {
-      throw new IllegalStateException(s"$this received more notifications than expected for transaction ${transaction}")
-    } else {
-      logger.trace(s"$this received a notification for transaction ${transaction}, ${stillPending} pending")
+      logger.trace(s"$this received last remaining notification for transaction ${ transaction }, starting reevaluation")
+      true
     }
-    //      }
+    else if (stillPending < 0) {
+      logger.error(s"$this received orphaned notification after having pulsed; ignoring notification")
+      false
+    }
+    else {
+      logger.trace(s"$this received a notification for transaction ${ transaction }, ${ stillPending } pending")
+      false
+    }
+  }
+
+  override def ping(transaction: Transaction, sourceDependenciesChanged: Boolean, pulsed: Boolean): Unit = {
+    val tx = transaction.stmTx
+    val pulseNow = tx.synchronized {
+      if (sourceDependenciesChanged) anyDependenciesChanged.set(true)(tx)
+      if (pulsed) anyPulse.set(true)(tx)
+      checkIfDependenciesPulsed(transaction)
+    }
+    if (pulseNow) {
+      val (dependenciesChangedFlag, pulsedFlag) = tx.synchronized {
+        (anyDependenciesChanged.swap(false)(tx), anyPulse.swap(false)(tx))
+      }
+      doReevaluation(transaction, dependenciesChangedFlag, pulsedFlag)
+    }
   }
 
   override protected def calculateSourceDependencies(tx: InTxn): Set[UUID] = {
