697a2191904e3c91350d31bed0f3016c7a9f04ee
ragnar <ragnar@rmgk.de>
2014-06-01 22:13:35 +0200

    rename construction time transaction for clarity

--- Core/src/main/scala/reactive/events/impl/DeltaEventStream.scala
+++ Core/src/main/scala/reactive/events/impl/DeltaEventStream.scala
@@ -6,7 +6,7 @@ import reactive.signals.Signal
 import reactive.impl.SingleDependentReactive
 import scala.concurrent.stm._
 
-class DeltaEventStream[A](val dependency: Signal[A], tx: InTxn) extends SingleDependentReactive(tx) with DependentEventStreamImpl[(A, A)] {
+class DeltaEventStream[A](val dependency: Signal[A], constructionTransaction: InTxn) extends SingleDependentReactive(constructionTransaction) with DependentEventStreamImpl[(A, A)] {
   private val lastValue = Ref(dependency.single.now)
   protected def reevaluate(tx: InTxn): Option[(A, A)] = {
       val newValue = dependency.pulse(tx).asOption.get
