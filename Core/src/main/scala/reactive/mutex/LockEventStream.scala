5afe44d3eaa0107cf2549546c4d22f8fd8adc147
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-06-05 10:24:35 +0200

    probably fixed lock reactives

--- Core/src/main/scala/reactive/mutex/LockEventStream.scala
+++ Core/src/main/scala/reactive/mutex/LockEventStream.scala
@@ -6,18 +6,13 @@ import reactive.impl.SingleDependentReactive
 import reactive.Transaction
 
 class LockEventStream[A](val dependency: EventStream[A], lock: TransactionLock) extends DependentEventStreamImpl[A] with SingleDependentReactive {
-  var isLocked = false
   protected def reevaluate(transaction: Transaction): Option[A] = {
-    isLocked = true
-    lock.acquire(transaction.uuid)
     dependency.pulse(transaction)
   }
 
   override protected[reactive] def doPulse(transaction: Transaction, sourceDependenciesChanged: Boolean, pulse: Option[A]) {
     super.doPulse(transaction, sourceDependenciesChanged, pulse)
-    if (isLocked) {
-      isLocked = false
-      lock.release(transaction.uuid)
-    }
+    lock.acquire(transaction.uuid)
+    lock.release(transaction.uuid)
   }
 }
--- Core/src/main/scala/reactive/mutex/LockSignal.scala
+++ Core/src/main/scala/reactive/mutex/LockSignal.scala
@@ -6,21 +6,14 @@ import reactive.impl.SingleDependentReactive
 import reactive.Transaction
 
 class LockSignal[A](override val dependency: Signal[A], lock: TransactionLock) extends DependentSignalImpl[A] with SingleDependentReactive {
-  var isLocked = false
   protected def reevaluateValue(transaction: Transaction): A = {
-    if (transaction != null) {
-      isLocked = true
-      lock.acquire(transaction.uuid)
-    }
     dependency.value(transaction)
   }
 
   override protected[reactive] def doPulse(transaction: Transaction, sourceDependenciesChanged: Boolean, pulse: Option[A]) {
+    lock.acquire(transaction.uuid)
     super.doPulse(transaction, sourceDependenciesChanged, pulse)
-    if (isLocked) {
-      isLocked = false
-      lock.release(transaction.uuid)
-    }
+    lock.release(transaction.uuid)
   }
 
 }
