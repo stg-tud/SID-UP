552b96ab245fa9ae1040b1704f630d764d85179c
ragnar <ragnar@rmgk.de>
2014-08-04 18:21:24 +0200

    propose interface to signal lifting with reversed parameters for type inference
    usage looks like
    `Signal(a, b, c) { (a, b, c) => a + b + c }`
    which might be better than
    `signal3 { (a: Int, b: Int, c: Int) => a + b + c }(a, b, c)`
    this also allows
    `Signal(a, b, c)(_ + _ + _)`

--- Core/src/main/scala/reactive/signals/Signal.scala
+++ Core/src/main/scala/reactive/signals/Signal.scala
@@ -13,12 +13,16 @@ trait Signal[+A] extends Reactive[A, A] {
   def delta(implicit inTxn: InTxn): EventStream[(A, A)]
   def flatMap[B](op: A => Signal[B])(implicit inTxn: InTxn): Signal[B]
   def tflatMap[B](op: (A, InTxn) => Signal[B])(implicit inTxn: InTxn): Signal[B]
-  def flatten[B](implicit evidence: A <:< Signal[B], inTxn: InTxn): Signal[B];
+  def flatten[B](implicit evidence: A <:< Signal[B], inTxn: InTxn): Signal[B]
   def snapshot(when: EventStream[_])(implicit inTxn: InTxn): Signal[A]
   def pulse(when: EventStream[_])(implicit inTxn: InTxn): EventStream[A]
 }
 
 object Signal {
+  def apply[A1, B](s1: Signal[A1])(f: A1 => B): Signal[B] = Lift.single.signal1(f)(s1)
+  def apply[A1, A2, B](s1: Signal[A1], s2: Signal[A2])(f: (A1, A2) => B): Signal[B] = Lift.single.signal2(f)(s1, s2)
+  def apply[A1, A2, A3, B](s1: Signal[A1], s2: Signal[A2], s3: Signal[A3])(f: (A1, A2, A3) => B): Signal[B]  = Lift.single.signal3(f)(s1, s2, s3)
+
   trait View[+A] extends Reactive.View[A] {
     def now: A
     def changes: EventStream[A]
@@ -26,7 +30,7 @@ object Signal {
     def tmap[B](op: (A, InTxn) => B): Signal[B]
     def delta: EventStream[(A, A)]
     def flatMap[B](op: A => Signal[B]): Signal[B]
-    def flatten[B](implicit evidence: A <:< Signal[B]): Signal[B];
+    def flatten[B](implicit evidence: A <:< Signal[B]): Signal[B]
     def snapshot(when: EventStream[_]): Signal[A]
     def pulse(when: EventStream[_]): EventStream[A]
   }
