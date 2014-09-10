9d76e2b4fb300da6724759f4a2c226f77901ca01
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-09-10 14:12:44 +0200

    removed erroneous import

--- Core/src/main/scala/reactive/events/impl/TransformEventStream.scala
+++ Core/src/main/scala/reactive/events/impl/TransformEventStream.scala
@@ -4,8 +4,6 @@ package impl
 
 import reactive.impl.SingleDependentReactive
 
-import scala.concurrent.stm.InTxn
-
 class TransformEventStream[A, B](val dependency: EventStream[B], private val op: Option[B] => Option[A]) extends DependentEventStreamImpl[A] with SingleDependentReactive {
   protected def reevaluate(transaction: Transaction): Option[A] = op(dependency.pulse(transaction))
 }
