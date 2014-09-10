40a743dc51c163cf8887ac9dceac472d00a6d292
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-09-10 14:02:48 +0200

    replaced 3 classes by 1 :) Conflicts: 	Core/src/main/scala/reactive/events/impl/EventStreamImpl.scala 	Core/src/main/scala/reactive/events/impl/FilteredEventStream.scala 	Core/src/main/scala/reactive/events/impl/MappedEventStream.scala 	Core/src/main/scala/reactive/events/impl/PartiallyMappedEventStream.scala

---
+++ Core/src/main/scala/reactive/events/impl/TransformEventStream.scala
@@ -0,0 +1,11 @@
+package reactive
+package events
+package impl
+
+import reactive.impl.SingleDependentReactive
+
+import scala.concurrent.stm.InTxn
+
+class TransformEventStream[A, B](val dependency: EventStream[B], private val op: Option[B] => Option[A]) extends DependentEventStreamImpl[A] with SingleDependentReactive {
+  protected def reevaluate(transaction: Transaction): Option[A] = op(dependency.pulse(transaction))
+}
