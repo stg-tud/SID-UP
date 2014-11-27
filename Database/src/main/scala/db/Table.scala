ad425f2b8afec1960e13010c5e3be2d246b9d72c
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-11-27 16:17:00 +0100

    new starting point for Table.select

--- Database/src/main/scala/db/Table.scala
+++ Database/src/main/scala/db/Table.scala
@@ -1,13 +1,17 @@
 package db
 
 import reactive.signals.{Signal, Var}
+import reactive.signals.TransposeSignal
 
-class Table[A](rows: A*) {
-  def select(where: (A) => Signal[Boolean]): Var[Set[A]] = {
-    Var(rows.filter {where(_).now}.toSet)
+class Table[A](rows: Var[Set[A]]) {
+  def select(where: A => Signal[Boolean]): Signal[Set[A]] = {
+    rows.map(_.map(row => where(row).map(_ -> row))).transposeS
+    
+    // TODO
+    ???
   }
 }
 
 object Table {
-  def apply[A](rows: A*): Table[A] = {new Table[A](rows: _*)}
+  def apply[A](rows: A*): Table[A] = {new Table[A](Var(rows.toSet))}
 }
