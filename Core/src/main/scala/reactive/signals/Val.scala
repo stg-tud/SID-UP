1c4ba5378b26586bce649288210a4828bc781868
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2014-12-12 12:15:33 +0100

    optimized transpose implementations on Val

--- Core/src/main/scala/reactive/signals/Val.scala
+++ Core/src/main/scala/reactive/signals/Val.scala
@@ -7,6 +7,9 @@ import scala.collection.generic.CanBuildFrom
 import scala.language.higherKinds
 import reactive.events.TransposeEventStream
 import scala.collection.TraversableLike
+import reactive.signals.impl.FunctionalSignal
+import reactive.events.impl.DependentEventStreamImpl
+import reactive.impl.MultiDependentReactive
 
 class Val[A](val value: A) extends Signal[A] with ReactiveConstant[A, A] {
   override val now = value
@@ -19,8 +22,14 @@ class Val[A](val value: A) extends Signal[A] with ReactiveConstant[A, A] {
   override def flatten[B](implicit evidence: A <:< Signal[B]): Signal[B] = value
   override def snapshot(when: EventStream[_]): Signal[A] = this
   override def pulse(when: EventStream[_]): EventStream[A] = when.map { _ => value }
-  override def transposeS[T, C[B] <: TraversableLike[B, C[B]]](implicit evidence: A <:< C[Signal[T]], canBuildFrom: CanBuildFrom[C[_], T, C[T]]) = new TransposeSignal[T, C](/*this.map(evidence)*/ this.asInstanceOf[Signal[C[Signal[T]]]])
-  override def transposeE[T, C[B] <: TraversableLike[B, C[B]]](implicit evidence: A <:< C[EventStream[T]], canBuildFrom: CanBuildFrom[C[_], T, C[T]]) = new TransposeEventStream[T, C](/*this.map(evidence)*/ this.asInstanceOf[Signal[C[EventStream[T]]]])
+  override def transposeS[T, C[B] <: TraversableLike[B, C[B]]](implicit evidence: A <:< C[Signal[T]], canBuildFrom: CanBuildFrom[C[_], T, C[T]]) = new FunctionalSignal({ transaction => evidence(value).map(_.value(transaction)) }, value.toSet)
+  override def transposeE[T, C[B] <: TraversableLike[B, C[B]]](implicit evidence: A <:< C[EventStream[T]], canBuildFrom: CanBuildFrom[C[_], T, C[T]]) = new DependentEventStreamImpl[C[T]] with MultiDependentReactive {
+    override val dependencies = value.toSet[Reactive[_, _]]
+    protected def reevaluate(transaction: Transaction): Option[C[T]] = {
+      val pulses = value.flatMap(_.pulse(transaction))
+      if (value.isEmpty) None else Some(pulses)
+    }
+  }
 }
 
 object Val {
--- Core/src/main/scala/reactive/signals/impl/FunctionalSignal.scala
+++ Core/src/main/scala/reactive/signals/impl/FunctionalSignal.scala
@@ -4,8 +4,6 @@ package impl
 
 import reactive.impl.MultiDependentReactive
 
-class FunctionalSignal[A](private val op: Transaction => A, private val inputs: Signal[_]*) extends {
-  override val dependencies = inputs.toSet[Reactive[_, _]]
-} with DependentSignalImpl[A] with MultiDependentReactive {
+class FunctionalSignal[A](private val op: Transaction => A, override val dependencies: Set[Reactive[_, _]]) extends DependentSignalImpl[A] with MultiDependentReactive {
   override def reevaluateValue(transaction: Transaction) = op(transaction)
 }
