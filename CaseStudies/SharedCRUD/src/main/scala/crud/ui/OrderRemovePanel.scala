e001a156350344b34760d76cfb16d35876ababd0
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2015-02-02 13:20:04 +0100

    removed EventSource in OrderRemovePanel; no idea why it still works though.

--- CaseStudies/SharedCRUD/src/main/scala/crud/ui/OrderRemovePanel.scala
+++ CaseStudies/SharedCRUD/src/main/scala/crud/ui/OrderRemovePanel.scala
@@ -1,35 +1,29 @@
 package crud.ui
 
-import javax.swing.{JOptionPane, BoxLayout, JLabel, JPanel}
+import javax.swing.{ JOptionPane, BoxLayout, JLabel, JPanel }
 
 import crud.data.Order
 import reactive.Lift._
 import reactive.events.EventSource
 import reactive.signals.Signal
-import ui.{ReactiveLabel, ReactiveButton}
+import ui.{ ReactiveLabel, ReactiveButton }
 
 class OrderRemovePanel(order: Signal[Option[Order]]) extends JPanel {
   protected val removeOrderButton = new ReactiveButton("Remove")
 
-  val removeOrders = EventSource[Set[Order]]()
-
-  // Assign order to removeOrders when the remove button is pressed
-  removeOrderButton.commits.observe { _ => order.now match {
-    case Some(o) => removeOrders << Set(o)
-    case None => JOptionPane.showMessageDialog(
+  val removeInstruction = order.pulse(removeOrderButton.commits)
+  removeInstruction.filter(_.isEmpty).observe { _ =>
+    JOptionPane.showMessageDialog(
       this,
       "Please select an order to delete",
       "No order selected",
-      JOptionPane.ERROR_MESSAGE
-    )
-  } }
+      JOptionPane.ERROR_MESSAGE)
+  }
+  val removeOrders = removeInstruction.collect { case Some(x) => Set(x) }
 
   // Enable/Disable button depending on whether or not an order is selected
-  order.map {
-    case Some(_) => removeOrderButton.asComponent.setEnabled(true)
-    case None => removeOrderButton.asComponent.setEnabled(false)
-  }
-  
+  removeOrderButton.enabled << order.map { _.isDefined }
+
   // Map selected order number to a label
   val orderNumber = order.map {
     case Some(o) => o.number.now
