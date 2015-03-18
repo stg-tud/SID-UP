40a96477607771a6e2d194a095e7af0b9deb57ba
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2015-03-18 11:33:09 +0100

    fixed date format string :)

--- CaseStudies/SharedCRUD/src/main/scala/crud/data/Order.scala
+++ CaseStudies/SharedCRUD/src/main/scala/crud/data/Order.scala
@@ -5,7 +5,7 @@ import java.util.Date
 import reactive.signals.{Signal, Var}
 
 case class Order(number: Signal[Int], date: Signal[Date]) {
-  val dateFormat = new SimpleDateFormat("yyy-MM-dd")
+  val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
 
   override def toString = {
     number.now + ": " + dateFormat.format(date.now)
