73d787571adcf634879e1ce55c1cedf618914ef2
Joscha Drechsler <drechsler@cs.tu-darmstadt.de>
2015-06-10 15:31:09 +0200

    extended ssort test

--- CaseStudies/SharedCRUD/src/test/scala/sort/SortTest.scala
+++ CaseStudies/SharedCRUD/src/test/scala/sort/SortTest.scala
@@ -2,6 +2,7 @@ package sort
 
 import org.scalatest.FunSuite
 import reactive.signals.Var
+import reactive.signals.Signal
 
 class SortTest extends FunSuite {
   val sort = new Sort[Int]
@@ -50,15 +51,34 @@ class SortTest extends FunSuite {
   }
 
   test("ssort works") {
-    val list = Var(List(Var(5), Var(2), Var(1), Var(4), Var(3)))
-    var expectedList = List(Var(1), Var(2), Var(3), Var(4), Var(5))
-    val sortedList = sort.ssort(list)
-
-    assert(expectedList.map(_.now) === sortedList.now.map(_.now))
-
-    list << List(Var(12), Var(43), Var(3), Var(26), Var(11))
-    expectedList = List(Var(3), Var(11), Var(12), Var(26), Var(43))
-
-    assert(expectedList.map(_.now) === sortedList.now.map(_.now))
+    
+    val a = Var(2)
+    val b = Var(4)
+    val c = Var(6)
+    val d = Var(8)
+    val e = Var(10)
+    
+    val list: Var[List[Signal[Int]]] = Var(List(e, d, c, b, a))
+    val sorted = sort.ssort(list)
+    def expect() = {
+      assert(list.now.map(_.now).sorted === sorted.now.map(_.now))
+    }
+    
+    expect()
+    
+    a << 100
+    expect()
+    
+    c << 4
+    expect()
+    
+    list << List(a, b, c, d)
+    expect()
+    
+    list << List(a.map(_ * 2), b, c, e.map(5 - _))
+    expect()
+    
+    e << 0
+    expect()
   }
 }
