d8598bffb34786631e349ecbd1812eb159ad8431
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2015-06-25 21:46:30 +0200

    Implemented ROrdering, rmin, rmax and a non-workign version of rsort1

--- CaseStudies/SharedCRUD/src/main/scala/reactive/sort/Sort.scala
+++ CaseStudies/SharedCRUD/src/main/scala/reactive/sort/Sort.scala
@@ -1,10 +1,7 @@
 package reactive.sort
 
 import reactive.lifting.Lift
-import reactive.signals.Signal
-import reactive.signals.impl.FunctionalSignal
-import reactive.Transaction
-import reactive.signals.Signal
+import reactive.signals.{Var, Signal}
 
 class Sort[T](implicit ordering: Ordering[T]) {
   def min(x: Signal[T], y: Signal[T]): Signal[T] = {
@@ -35,17 +32,46 @@ class Sort[T](implicit ordering: Ordering[T]) {
   def ssort(list: Signal[List[Signal[T]]]): Signal[List[Signal[T]]] = list.map(sort)
 }
 
-trait ROrdering[T]
-abstract class RSort[T](implicit ordering: ROrdering[T]) {
-  def rmin(x: T, y: T): Signal[T]
+trait ROrdering[T] {
+  def compare(a: T, b: T): Signal[Int]
+}
+
+class RSort[T](implicit ordering: ROrdering[T]) {
+  def rmin(x: T, y: T): Signal[T] = {
+    ordering.compare(x, y).map { comp => if (comp < 0) x else y }
+  }
+
+  def rmax(x: T, y: T): Signal[T] = {
+    ordering.compare(x, y).map { comp => if (comp > 0) x else y }
+  }
+
   def rrmin(x: Signal[T], y: Signal[T]): Signal[T] = {
     Lift.signal2(rmin)(x, y).flatten
   }
-  def rsort1(list: List[T]): List[Signal[T]]
+
+  def rrmax(x: Signal[T], y: Signal[T]): Signal[T] = {
+    Lift.signal2(rmax)(x, y).flatten
+  }
+
+  def rsort1(list: List[T]): List[Signal[T]] = {
+    // Bring the minimum element to the front of the list
+    def bubbleUp(list: List[Signal[T]]): List[Signal[T]] = list.tail.foldLeft(List(list.head)) { (l, x) =>
+      rrmin(l.head, x) :: rrmax(l.head, x) :: l.tail
+    }
+
+    val signalList = list.map(Var(_))
+    signalList.size match {
+      case 0 => signalList
+      case 1 => signalList
+      case _ => val l = bubbleUp(signalList); l.head :: rsort1(l.tail.map(_.now))
+    }
+  }
+
   def rsort2(list: List[T]): Signal[List[T]] = {
     import Signal._
     rsort1(list).signalTranspose
   }
+
   def rrsort(signal: Signal[List[T]]): Signal[List[T]] = {
     signal.map(rsort1).transposeS
   }
--- CaseStudies/SharedCRUD/src/test/scala/reactive/sort/SortTest.scala
+++ CaseStudies/SharedCRUD/src/test/scala/reactive/sort/SortTest.scala
@@ -1,11 +1,23 @@
 package reactive.sort
 
 import org.scalatest.FunSuite
+import reactive.lifting.Lift
 import reactive.signals.Var
 import reactive.signals.Signal
 
 class SortTest extends FunSuite {
+  class SignalPairOrdering extends ROrdering[(Signal[Int], Signal[Int])] {
+    override def compare(a: (Signal[Int], Signal[Int]), b: (Signal[Int], Signal[Int])): Signal[Int] = {
+      val asum = Lift.signal2 {(x:Int, y:Int) => x+y } (a._1, a._2)
+      val bsum = Lift.signal2 {(x:Int, y:Int) => x+y } (b._1, b._2)
+
+      Lift.signal2(Ordering[Int].compare)(asum, bsum)
+    }
+  }
+  implicit val pairOrdering = new SignalPairOrdering
+
   val sort = new Sort[Int]
+  val rsort = new RSort[(Signal[Int], Signal[Int])]
 
   test("min works") {
     val x: Var[Int] = Var(1)
@@ -45,7 +57,6 @@ class SortTest extends FunSuite {
     val list = List(Var(5), Var(2), Var(1), Var(4), Var(3))
     val expectedList = List(Var(1), Var(2), Var(3), Var(4), Var(5))
     val sortedList = sort.sort(list)
-    sortedList.foreach { s => print(s.now + ", ")}
 
     assert(expectedList.map(_.now) === sortedList.map(_.now))
   }
@@ -80,5 +91,92 @@ class SortTest extends FunSuite {
     
     e << 0
     expect()
+  }
+
+  test("ROrdering works") {
+    val a = (Var(1), Var(2))
+    val b = (Var(3), Var(4))
+    val ordering = new SignalPairOrdering()
+    assert(ordering.compare(a, b).now === -1)
+
+    a._1 << 6
+    assert(ordering.compare(a, b).now === 1)
+
+    b._2 << 5
+    assert(ordering.compare(a, b).now === 0)
+  }
+
+  test("rmin works") {
+    val x = (Var(1), Var(2))
+    val y = (Var(3), Var(4))
+    val min = rsort.rmin(x, y)
+
+    // x = (1, 2), y = (3, 4)
+    assert((1, 2) === (min.now._1.now, min.now._2.now))
+
+    // x = (1, 2), y = (0, 1)
+    y._1 << 0
+    y._2 << 1
+    assert((0, 1) === (min.now._1.now, min.now._2.now))
+
+    // x = (0, 1), y = (0, 1)
+    x._1 << 0
+    x._2 << 1
+    assert((0, 1) === (min.now._1.now, min.now._2.now))
+  }
+
+  test("rmax works") {
+    val x = (Var(1), Var(2))
+    val y = (Var(3), Var(4))
+    val max = rsort.rmax(x, y)
+
+    // x = (1, 2), y = (3, 4)
+    assert((3, 4) === (max.now._1.now, max.now._2.now))
+
+    // x = (1, 2), y = (0, 1)
+    y._1 << 0
+    y._2 << 1
+    assert((1, 2) === (max.now._1.now, max.now._2.now))
+
+    // x = (0, 1), y = (0, 1)
+    x._1 << 0
+    x._2 << 1
+    assert((0, 1) === (max.now._1.now, max.now._2.now))
+  }
+
+  test("rsort1 works") {
+    implicit val ord = new Ordering[(Int, Int)] {
+      override def compare(x: (Int, Int), y: (Int, Int)): Int = Ordering[Int].compare(x._1+x._2, y._1+y._2)
+    }
+
+    val a = (Var(1), Var(2))
+    val b = (Var(3), Var(4))
+    val c = (Var(5), Var(6))
+    val d = (Var(7), Var(8))
+    val e = (Var(9), Var(10))
+
+    val list: List[(Signal[Int], Signal[Int])] = List(e, d, c, b, a)
+    val sorted = rsort.rsort1(list)
+    def expect() = {
+      assert(list.map(s => (s._1.now, s._2.now)).sorted === sorted.map(s => (s.now._1.now, s.now._2.now)))
+    }
+
+    expect()
+
+    a._1 << 5
+    a._2 << 3
+    expect()
+//
+//    c << 4
+//    expect()
+//
+//    list << List(a, b, c, d)
+//    expect()
+//
+//    list << List(a.map(_ * 2), b, c, e.map(5 - _))
+//    expect()
+//
+//    e << 0
+//    expect()
   }
 }
