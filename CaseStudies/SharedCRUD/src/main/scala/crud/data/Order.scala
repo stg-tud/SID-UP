06b1df3faf87007639bba8db7a4886a6bf9367be
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2015-07-09 19:48:07 +0200

    Introduce uuid for orders as workaround for remote deletes

--- CaseStudies/SharedCRUD/src/main/scala/crud/data/Order.scala
+++ CaseStudies/SharedCRUD/src/main/scala/crud/data/Order.scala
@@ -1,7 +1,7 @@
 package crud.data
 
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.{UUID, Date}
 
 import reactive.signals.SettableSignal
 
@@ -10,6 +10,15 @@ case class Order(number: SettableSignal[Int], date: SettableSignal[Date]) {
 
   override def toString = {
     number.now + ": " + dateFormat.format(date.now)
+  }
+
+  // TODO this is a workaround to allow equals to work with remote references
+  val uuid = UUID.randomUUID()
+  override def equals(other: Any): Boolean = {
+    if (other.isInstanceOf[Order]) {
+        return other.asInstanceOf[Order].uuid == this.uuid
+    }
+    super.equals(other)
   }
 }
 object Order {
